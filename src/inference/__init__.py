3e84c5aa617d6a5bb56af06f2e55045394876291
Дмитриев Андрей <62550256+agd01991@users.noreply.github.com>
2026-05-03 11:57:25 +0300

    Export YOLO and RT-DETR inference adapters

--- src/inference/__init__.py
+++ src/inference/__init__.py
@@ -5,5 +5,14 @@ This package contains adapters that convert outputs of different models
 """
 
 from .prediction import DetectionPrediction, ImagePrediction
+from .rtdetr_inference import predict_rtdetr_folder, predict_rtdetr_image
+from .yolo_inference import predict_yolo_folder, predict_yolo_image
 
-__all__ = ["DetectionPrediction", "ImagePrediction"]
+__all__ = [
+    "DetectionPrediction",
+    "ImagePrediction",
+    "predict_yolo_image",
+    "predict_yolo_folder",
+    "predict_rtdetr_image",
+    "predict_rtdetr_folder",
+]
