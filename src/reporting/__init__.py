156928be8aa0326b3dbafeda727ee305a2b2c7d2
Дмитриев Андрей <62550256+agd01991@users.noreply.github.com>
2026-06-01 23:07:35 +0300

    Export segmentation-identification report helper

--- src/reporting/__init__.py
+++ src/reporting/__init__.py
@@ -5,5 +5,6 @@ from inference, evaluation, comparison and density analysis artifacts.
 """
 
 from .mini_report import build_mini_report
+from .segmentation_identification_report import generate_segmentation_identification_report
 
-__all__ = ["build_mini_report"]
+__all__ = ["build_mini_report", "generate_segmentation_identification_report"]
